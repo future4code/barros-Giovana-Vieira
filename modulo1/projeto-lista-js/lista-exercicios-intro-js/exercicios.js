3f27ea5182d97a9231de80e6ea22e5acb56e8404
Giovana Inez Vieira <gioivieira@gmail.com>
2022-04-21 23:04:10 -0300

    Terceiras alterações no projeto-lista-js

--- modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
+++ modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
@@ -118,18 +118,57 @@ function checaIgualdadeDesconsiderandoCase(string1, string2) {
 }
 
 // EXERCÍCIO 13
+/*- Exercício 13
+    
+    Escreva uma função que pergunta ao usuário o ano atual, o ano de nascimento de uma pessoa, 
+    e o ano em que sua carteira de identidade foi emitida (nessa ordem). A função deve imprimir
+     no console um booleano (true ou false) que indica se a carteira precisa ser renovada ou não.
+      A carteira precisa ser renovada segundo os seguintes critérios:
+    
+    - Para pessoas com menos de 20 anos, ou exatamente 20 anos, deve ser renovada 
+    de 5 em 5 anos (se for exatamente 5 anos, deve ser renovada).
+    - Para pessoas entre 20 e 50 anos, ou exatamente 50, deve ser renovada 
+    de 10 em 10 anos (se for exatamente 10 anos, deve ser renovada).
+    - Para pessoas acima dos 50 anos, deve ser renovada de 15 em 15 anos*/
+
 function checaRenovacaoRG() {
+  let anoAtual = Number(prompt("Qual o ano atual?"))
+  let anoDeNascimento = Number(prompt("Qual o ano do nascimento?"))
+  let carteira = Number(prompt("Qual o ano em que sua carteira foi emitida?"))
+  let idade = anoAtual - anoDeNascimento
+  let renovacao = anoAtual - carteira
+  let menosVinte = (idade <= 20) === (renovacao >= 5)
+  let entreVinteCinquenta = ((idade > 20) || (idade <= 50) === (renovacao >= 10))
+  let acimaCinquenta = (idade > 50) === (renovacao >= 15)
+  console.log(menosVinte || entreVinteCinquenta || acimaCinquenta)
     // implemente sua lógica aqui
 }
 
+
+
 // EXERCÍCIO 14
+/*- Exercício 14
+    
+    Escreva uma função que recebe um ano e retorna um booleano (true ou false) 
+    que indica se o ano é bissexto. Um ano é bissexto de acordo com as seguintes condições:
+    
+    - **São bissextos** todos os anos múltiplos de 400**.**
+    - **São bissextos** todos os múltiplos de 4, exceto se for múltiplo de 100 mas não de 400**.**
+    - **Não são bissextos** todos os demais anos.*/
+
 function checaAnoBissexto(ano) {
   // implemente sua lógica aqui
 
 }
 
 // EXERCÍCIO 15
-function checaValidadeInscricaoLabenu() {
-  // implemente sua lógica aqui
 
+function checaValidadeInscricaoLabenu() {
+
+  let maisDeDezoito = prompt("Você tem mais de 18 anos?")
+  let esinoMedio = prompt("Você possui ensino médio completo?")
+  let disponibilidade = prompt("Você possui disponibilidade exclusiva durante os horários do curso?")
+  let resposta = "sim"
+  console.log((maisDeDezoito === resposta) && (esinoMedio === resposta) && (disponibilidade === resposta))
+  // implemente sua lógica aqui
 }
