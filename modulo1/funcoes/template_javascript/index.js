3d8f58611b50f757ec1c831b7164a3915aa10191
Giovana Inez Vieira <gioivieira@gmail.com>
2022-04-22 19:15:06 -0300

    Alterações finais

--- modulo1/funcoes/template_javascript/index.js
+++ modulo1/funcoes/template_javascript/index.js
@@ -17,15 +17,15 @@ iii. cenouras crescem na terra / true
 // Exercício 1:
 // a)
 
-/*function dados() {
+function dados() {
     console.log("Eu sou Giovana, tenho 23 anos, moro em São Sebastião do Paraíso e sou estudante.")
 }
 
-dados()*/
+dados()
 
 // b)
 
-/*function informacoes(nome , idade , cidade , profissao) {
+function informacoes(nome , idade , cidade , profissao) {
     nome = prompt("Qual seu nome?")
     idade = prompt("Qual sua idade?")
     cidade = prompt("Qual sua cidade?")
@@ -33,54 +33,54 @@ dados()*/
     return `Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}.`
 }
 
-console.log(informacoes())*/
+console.log(informacoes())
 
 // Exercício 2:
 // a)
 
-/*function soma(num1 , num2) {
+function soma(num1 , num2) {
     num1 = 2
     num2 = 3
     let somar = (num1 + num2)
     return somar
 }
 
-console.log(soma())*/
+console.log(soma())
 
 // b)
 
-/*function maiorOuMenor(num1 , num2) {
+function maiorOuMenor(num1 , num2) {
     num1 = (prompt("Digite um número:"))
     num2 = (prompt("Digite outro número:"))
     return num1 >= num2
 }
 
-console.log(maiorOuMenor())*/
+console.log(maiorOuMenor())
 
 // c)
 
-/*function par(num) {
+function par(num) {
     num = Number(prompt("Escreva um número:"))
     let numero = 0
     let parOuNao = (num % 2) == numero
     return parOuNao  
 }
 
-console.log(par())*/
+console.log(par())
 
 // d)
 
-/*function string(string1) {
+function string(string1) {
     string1 = prompt("Escreva uma mensagem:")
     console.log(string1.length)
     console.log(string1.toUpperCase())
 }
 
-string()*/
+string()
 
 // Exercício 3:
 
-/*let um = Number(prompt("Digite um número:"))
+let um = Number(prompt("Digite um número:"))
 let dois = Number(prompt("Digite outro número:"))
 
 function soma1() {
@@ -99,42 +99,36 @@ function divisao() {
     return um / dois    
 }
 
+console.log(um , dois)
 console.log(soma1())
 console.log(subtracao())
 console.log(multiplicacao())
-console.log(divisao())*/
+console.log(divisao())
 
-/* Desafio 1:
+// Desafio 1:
 
-1. Funções são trechos de códigos como quaisquer outros mas que podemos acessá-los mais de uma vez ao 
-longo do código através de invocações/chamadas. Então, funções podem chamar/invocar outras funções também.
- Sua tarefa é escrever duas funções
-    
-    a) Escreva uma *arrow function* que recebe um parâmetro e imprime no console esse parâmetro
-    
-    b) Escreva outra *arrow function* que recebe dois valores como parâmetros mas **nenhum retorno.** 
-    Faça a soma entre esses valores e chame a sua primeira função mandando este resultado da soma como 
-    entrada para imprimi-lo
-
-
-// a) 
-
-let arrow1 = (parametroUm) => {
+let arrowUm = (parametroUm) => {
     console.log(parametroUm)
 }
 
-// b/
-
-
-let arrow2 = (parametroUm , parametroDois) => {
-    parametroUm = 20
-    parametroDois = 30
-    let somaUm = parametroUm + parametroDois
-    arrow1 = somaUm
-
+let arrowDois = (parametroDois , parametroTres) => {
+    arrowUm(parametroDois + parametroTres)
 }
 
-console.log(arrow1()) */
+
+arrowDois(2 , 5)
+
+// Desafio 2:
+
+let teorema = (catetoUm , catetoDois) => {
+    catetoUm = Number(prompt("Cateto 1:"))
+    catetoDois = Number(prompt("Cateto 2:"))
+    let hipotenusa = Math.sqrt(catetoUm ** 2 + catetoDois ** 2)
+    return hipotenusa
+}
+
+console.log(teorema(2 , 3))
+ 
 
 
 
