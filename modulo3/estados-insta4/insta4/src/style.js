69fde16b85764924abcb4ebf33018785570726d9
Giovana Inez Vieira <gioivieira@gmail.com>
2022-07-05 16:06:37 -0300

    Exercício renderização de listas

---
+++ modulo3/estados-insta4/insta4/src/style.js
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const IconContainer = styled.div`
+    display: flex;
+`
+
+export const IconImage = styled.img ` 
+	margin-right: 5px;
+`
+
+export const CommentContainer = styled.div `
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+`
+
+export const InputComentario = styled.input `
+    width: 100%;
+    margin-right: 5px;
+`
+
+export const PostContainer = styled.div `
+    border: 1px solid gray;
+    width: 300px;
+    margin-bottom: 10px;
+`
+
+export const PostHeader = styled.div `
+    height: 40px;
+    display: flex;
+    align-items: center;
+    padding-left: 10px;
+`
+
+export const UserPhoto = styled.img `
+    height: 30px;
+    width: 30px;
+    margin-right: 10px;
+    border-radius: 50%;
+`
+
+export const PostPhoto = styled.img `
+    width: 100%;
+`
+
+export const PostFooter = styled.div`
+    height: 40px;
+    display: flex;
+    align-items: center;
+    padding: 0 10px;
+    justify-content: space-between;
+`
