4852b67ecd78e17955f22992d6de2d30d6cb0a04
Giovana Inez Vieira <gioivieira@gmail.com>
2022-07-14 17:39:58 -0300

    Exercício Thinking React

---
+++ modulo3/thinking-react/MockDeDados.js
@@ -0,0 +1,8 @@
+export const Produtos = [
+    {id: 1, name: "Produto 1", price: 200, photo: "https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY"},
+    {id: 2, name: "Produto 2", price: 300, photo: "https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY"},
+    {id: 3, name: "Produto 3", price: 250, photo: "https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY"},
+    {id: 4, name: "Produto 4", price: 100, photo: "https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY"},
+    {id: 5, name: "Produto 5", price: 150, photo: "https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY"},
+    {id: 6, name: "Produto 6", price: 400, photo: "https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY"}
+]
---
+++ modulo3/thinking-react/README.md
@@ -0,0 +1,12 @@
+## IMAGEM DOS COMPONENTES:
+![Imagem dos componentes]
+(IMAGEM COMPONENTES.jpg)
+
+
+## DIAGRAMA DOS COMPONENTES:
+![Diagrama dos componentes]
+(DIAGRAMA COMPONENTES.jpg)
+
+## DIAGRAMA DOS ESTADOS DENTRO DOS COMPONENTES:
+![Diagrama dos estados dentro dos componentes]
+(DIAGRAMA ESTADOS.jpg)
