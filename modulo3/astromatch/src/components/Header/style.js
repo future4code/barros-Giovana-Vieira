aa9d61274b4cdac1a8c216bd4ee3d6063f3099be
Giovana Inez Vieira <gioivieira@gmail.com>
2022-08-03 16:11:10 -0300

    Segundas alterações projeto-astromatch

--- modulo3/astromatch/src/components/Header/style.js
+++ modulo3/astromatch/src/components/Header/style.js
@@ -1,7 +1,26 @@
 import styled from "styled-components";
 
 export const Header = styled.header`
-    background-color: lightgray;    
+    display: flex;
+    border: solid 0.3vw lightblue;
+    justify-content: space-around;
+    align-items: center;
     text-align: center;
-    padding: 2vw;
+    padding: 1vw;
+
+
+    button {
+        img{
+            width: 3vw;
+            background-color: white;
+        }
+    }
+
+    p {
+        font-size: 1.8vw;
+        font-weight: bold;
+        span {
+            color: green;
+        }
+    }
 `
--- modulo3/astromatch/src/style.js
+++ modulo3/astromatch/src/style.js
@@ -2,18 +2,52 @@ import styled from "styled-components";
 
 export const ContainerPrincipal = styled.div `
     display: grid;
-    grid-template-columns: 1fr 2fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
 `
-
 export const AstroMatch = styled.div`
     display: grid;
-    grid-template-rows: 1fr 6fr 1fr;
+    grid-template-rows: 1fr 6fr 1fr
 
     main {
-        height: 75vh;
+        display: grid;
+        grid-template-rows: 6.5fr 1fr;
     }
 `
-
 export const Card = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1vw;
+   
+    img {
+        width: 30vw;
+        height: 55vh;
+        background-color: green;
+        margin-bottom: 0.5vw;
+    }
 
+    h1{
+        margin-bottom: 0.5vw;
+        font-size: 2vw;
+        span{
+            margin-bottom: 0.5vw;
+            font-size: 2vw;
+        }
+    }
+
+    span{
+        margin-bottom: 0.5vw;
+        font-size: 1vw;
+    }
+`
+export const Botoes = styled.section`
+    display: flex;
+    justify-content: space-around;
+    
+    button{
+        img{
+            width: 6vw;
+            background-color: white;
+        }
+    }
 `
