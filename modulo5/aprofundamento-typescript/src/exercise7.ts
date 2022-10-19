aead20c9cb7e19e4e407b57f1a43889f69d0067a
Giovana Inez Vieira <gioivieira@gmail.com>
2022-10-19 15:46:45 -0300

    Tentativa de realizar a letra a do exercício 7 de aprofundamento.

--- modulo5/aprofundamento-typescript/src/exercise7.ts
+++ modulo5/aprofundamento-typescript/src/exercise7.ts
@@ -1,6 +1,4 @@
-/* Um parente seu decidiu abrir um restaurante; e, recentemente, ele descobriu que você está fazendo um curso de programação. 
-Além de pedir para você arrumar alguns computadores antigos dele, configurar a internet e outros pedidos clássicos, 
-ele prometeu te pagar caso você implementasse um sistema para o restaurante. Os pratos deste restaurante possuem  um nome, um custo, um valor de venda,
+/* Os pratos deste restaurante possuem  um nome, um custo, um valor de venda,
 e um array de ingredientes. Cada uma das vendas deve conter o nome do prato e o nome do cliente que realizou a compra.
 
 a) Escreva uma função que permita cadastrar um produto. Salve os produtos em um array no escopo global.
@@ -10,3 +8,35 @@ b) Escreva uma função que recebe um nome e devolve o valor do produto com este
 c) Escreva uma função para que ele venda um prato. Salve as vendas em um array no escopo global.
 
 d) Escreva uma função que determine o lucro do restaurante. */
+
+type Dishs = {
+    dishName: string,
+    cost: number,
+    price: number,
+    ingredientes: string[]
+}
+
+type Sales = {
+    dishName: string,
+    clientName: string
+}
+
+let newProducts: Dishs[] = []
+
+const addProducts = () => {
+
+    let newProduct: Dishs = {
+        dishName: "Risoto",
+        cost: 40,
+        price: 60,
+        ingredientes: ['Vinho Branco', 'Legumes', 'Arroz', 'Manteiga']
+    }
+
+    newProducts.push(...newProducts, newProduct)
+    
+}
+
+addProducts()
+
+console.log(newProducts)
+
