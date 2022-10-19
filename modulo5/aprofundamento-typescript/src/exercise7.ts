6442f111513a78e5e28aa5e8264c05a89e54524f
Giovana Inez Vieira <gioivieira@gmail.com>
2022-10-19 16:49:50 -0300

    Finalização da letra a, b e c do exercício 7 de aprofundamento e início da resolução da letra d.

--- modulo5/aprofundamento-typescript/src/exercise7.ts
+++ modulo5/aprofundamento-typescript/src/exercise7.ts
@@ -21,7 +21,22 @@ type Sales = {
     clientName: string
 }
 
-let newProducts: Dishs[] = []
+let products: Dishs[] = [
+    {
+        dishName: "Pudim",
+        cost: 20,
+        price: 35,
+        ingredientes: ['Leite Condensado', 'Leite', 'Ovos', 'Açucar']
+    },
+    {
+        dishName: "Macarrão alho e óleo",
+        cost: 30,
+        price: 45,
+        ingredientes: ['Macarrão', 'Alho', 'Azeite', 'Pimenta']
+    }
+]
+
+// a)
 
 const addProducts = () => {
 
@@ -32,11 +47,73 @@ const addProducts = () => {
         ingredientes: ['Vinho Branco', 'Legumes', 'Arroz', 'Manteiga']
     }
 
-    newProducts.push(...newProducts, newProduct)
+    products.push(newProduct)
     
 }
 
-addProducts()
+/* addProducts()
+console.log(products) */
 
-console.log(newProducts)
+// b)
 
+const returnNameOfTheProduct = (dishName: string): number => {
+
+    const productPrice = products.filter((product)=>{
+       if(dishName === product.dishName){
+         return product.price
+       }
+    })
+
+    for(let product of productPrice){
+        return product.price
+    }
+}
+
+/* console.log(returnNameOfTheProduct("Macarrão alho e óleo")) */
+
+// c)
+
+let sales: Sales[] = [
+    {
+        dishName: "Pudim",
+        clientName: "Giovana"
+    },
+    {
+        dishName: "Macarrão alho e óleo",
+        clientName: "Júlia"
+    }
+]
+
+const addSales = () => {
+
+    let newSale: Sales = {
+        dishName: "Risoto",
+        clientName: "Davi"
+    }
+
+    sales.push(newSale)
+}
+
+/* addSales()
+console.log(sales) */
+
+// d)
+
+const profit = (): void => {
+
+    let sale;
+    let product;
+    let restaurantProfit;
+
+    for(sale of sales){
+        for(product of products){
+            if(product.dishName === sale.dishName){
+                restaurantProfit = product.price - product.cost
+            } 
+        }
+    }
+
+    console.log(restaurantProfit)
+}
+
+profit()
