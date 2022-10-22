7a4f215c18718979084a5c66a9d2d5256bb775a5
Giovana Inez Vieira <gioivieira@gmail.com>
2022-10-22 09:52:04 -0300

    Resolução do exercício 9 da lista de typescript.

--- modulo5/lista-typescript/src/exercise9.ts
+++ modulo5/lista-typescript/src/exercise9.ts
@@ -1,94 +1,64 @@
-/* O CPF consiste de 11 dígitos cuja configuração respeita o formato XXX.XXX.XXX-XX. Para a construção de um número de CPF as seguintes regras são aplicadas.
-
-- Os oito primeiros dígitos são os número-base
-- O nono dígito é a região fiscal
-- O penúltimo dígito é o dígito verificador (DV) módulo 11 dos nove dígitos anteriores
-- O último dígito é o DV módulo 11 dos dez dígitos anteriores
-
-Para o cálculo dos DV existem pesos associados a cada dígito, abaixo segue exemplo do cálculo dos DV do CPF com número-base 145382206.
-
-- Exemplo de cálculo do primeiro Dígito Verificador
-    
-    Começamos utilizando os 9 primeiros dígitos multiplicando-os pela sequência decrescente de 10 à 2 e somamos esse resultado.
-    
-    ```
-    1  4  5  3  8  2  2  0  6
-    x  x  x  x  x  x  x  x  x
-    10 9  8  7  6  5  4  3  2
-    =  =  =  =  =  =  =  =  =
-    10 36 40 21 48 10 8  0  12
-    
-    10 + 36 + 40 + 21 + 48 + 10 + 8 + 0 + 12 = 185
-    ```
-    
-    Com esse resultado agora basta realizar a operação de módulo 11. Ou seja:
-    
-    ```
-    185 % 11 = 9
-    ```
-    
-    O resto da divisão é `9`. Agora para calcular o dígito verificador vamos subtrair este resto do número 11:
-    
-    ```
-    11 - 9 = 2
-    ```
-    
-    ATENÇÃO: Como o resultado da subtração foi 2, o primeiro dígito verificador é igual a 2. Caso o resultado dessa subtração for `10` ou maior, o penúltimo dígito verificador será o `0`.
-    
-- Exemplo de cálculo do segundo Dígito Verificador
-    
-    A validação do segundo dígito é semelhante a primeira, porém vamos considerar o primeiro dígito verificador calculado anteriormente. Por isso a multiplicação é feita de 11 à 2.
-    
-    ```
-    1  4  5  3  8  2  2  0  6  2
-    x  x  x  x  x  x  x  x  x  x
-    11 10 9  8  7  6  5  4  3  2
-    =  =  =  =  =  =  =  =  =  =
-    11 40 45 24 56 12 10 0  18 4
-    
-    11 + 40 + 45 + 24 + 56 + 12 + 10 + 0 + 18 + 4 = 220
-    ```
-    
-    Novamente vamos efetuar a divisão por 11 usando o módulo:
-    
-    ```
-    220 % 11 = 0
-    ```
-    
-    E vamos fazer a subtração:
-    
-    ```
-    11 - 0 = 11
-    ```
-    
-    Como o valor é igual ou maior que `10`, o último dígito é `0`.
-    
-    Assim, confirmamos os dois dígitos verificadores do nosso CPF 145.382.206-**20** e sabemos que esse CPF é válido. 
-    
-
-Outra regra muito importante é que CPFs com números iguais como: `111.111.111-11`, `222.222.222-22`, entre outros, são CPFs válidos pelo algoritmo 
-mas não existem no registro oficial. Assim esse tipo de CPF não pode ser usado.
-
- 
-Você deve criar uma função que receba o CPF no formato “xxx.xxx.xxx-xx” e faça uma validação do valor recebido. Caso o CPF recebido 
-seja válido retorne um `True` e caso seja inválido retorne `False` */
-
-const isValid = (cpf: string) => {
+const isValid = (cpf: string): boolean => {
 
     let cpfArray: string[] = cpf.split("")
     cpfArray.splice(3, 1)
     cpfArray.splice(6, 1)
     cpfArray.splice(9, 1)
     let cpfArrayNumbers: number[] = []
-    let multiplicator: number = 2;
-    let result;
     
-    for(let i = 0; i < cpfArray.length; i++){
-        cpfArrayNumbers.push(parseInt(cpfArray[i]))
+    for(let i = 0; i < cpfArray.length; i++) cpfArrayNumbers.push(parseInt(cpfArray[i]))
+    
+    if(cpfArrayNumbers.length != 11 || 
+    cpf === "000.000.000-00" 
+    || cpf === "111.111.111-11" 
+    || cpf === "222.222.222-22" 
+    || cpf === "333.333.333-33" 
+    || cpf === "444.444.444-44" 
+    || cpf === "555.555.555-55" 
+    || cpf === "666.666.666-66" 
+    || cpf === "777.777.777-77" 
+    || cpf === "888.888.888-88" 
+    || cpf === "999.999.999-99") {
+        return false
+    }  
+
+    let mult1 = cpfArrayNumbers[0] * 10
+    let mult2 = cpfArrayNumbers[1] * 9
+    let mult3 = cpfArrayNumbers[2] * 8
+    let mult4 = cpfArrayNumbers[3] * 7
+    let mult5 = cpfArrayNumbers[4] * 6
+    let mult6 = cpfArrayNumbers[5] * 5
+    let mult7 = cpfArrayNumbers[6] * 4
+    let mult8 = cpfArrayNumbers[7] * 3
+    let mult9 = cpfArrayNumbers[8] * 2
+
+    let sum1 = mult1 + mult2 + mult3 + mult4 + mult5 + mult6 + mult7 + mult8 + mult9
+    let dv1 = 11 - (sum1 % 11) 
+
+    if(dv1 >= 10) dv1 = 0
+
+    let mult10 = cpfArrayNumbers[0] * 11
+    let mult11 = cpfArrayNumbers[1] * 10
+    let mult12 = cpfArrayNumbers[2] * 9
+    let mult13 = cpfArrayNumbers[3] * 8
+    let mult14 = cpfArrayNumbers[4] * 7
+    let mult15 = cpfArrayNumbers[5] * 6
+    let mult16 = cpfArrayNumbers[6] * 5
+    let mult17 = cpfArrayNumbers[7] * 4
+    let mult18 = cpfArrayNumbers[8] * 3
+    let mult19 = cpfArrayNumbers[9] * 2
+
+    let sum2 = mult10 + mult11 + mult12 + mult13 + mult14 + mult15 + mult16 + mult17 + mult18 + mult19
+    let dv2 = 11 - (sum2 % 11)
+
+    if(dv2 >= 10) dv2 = 0
+
+    if(cpfArrayNumbers[9] === dv1 && cpfArrayNumbers[10] === dv2){
+        return true
+    } else {
+        return false
     }
-    
- 
-    
+
 }
 
-console.log(isValid("145.382.206-20"))
+console.log(isValid("446.867.888-70"))
