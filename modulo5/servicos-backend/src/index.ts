1e6fd7816c7e47c72eea380c7885c893092a35e7
Giovana Inez Vieira <gioivieira@gmail.com>
2022-11-29 17:48:54 -0300

    Resolução dos exercícios 1, 2 e 3.

--- modulo5/servicos-backend/src/index.ts
+++ modulo5/servicos-backend/src/index.ts
@@ -2,8 +2,8 @@ import { Request, Response } from "express"
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import connection from './dataBase/connection'
-import getAddressInfo from '../src/services/getAddressInfo'
+import { addressInfos } from "./requests"
+
 
 dotenv.config()
 
@@ -12,20 +12,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.post('/address', async (req: Request, res: Response): Promise<void> => {
-    
-    const {cep} = req.body
-    let statusCode = 400
-
-    try {
-        const result = await getAddressInfo(cep)
-
-        res.status(201).send(result)
-    } catch (err: any) {
-        res.status(statusCode).send(err.message)
-    }
-
-})
+app.post('/address', addressInfos)
 
 app.listen(process.env.PORT || 3003, ()=>{
     console.log(`Server is running at port ${process.env.PORT || 3003}.`)
--- modulo5/servicos-backend/src/services/getAddressInfos.ts
+++ modulo5/servicos-backend/src/services/getAddressInfos.ts
@@ -1,9 +1,8 @@
 import axios from "axios"
 
-const getAddressInfo = async (cep: string) => {
+export const getAddressInfos = async (cep: string) => {
     const {data} = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
 
     return data
 }
 
-export default getAddressInfo;
