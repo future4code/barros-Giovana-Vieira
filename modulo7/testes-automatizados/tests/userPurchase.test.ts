fb11bb19ee8d779d161ad248850a01458ed99f7e
Giovana Inez Vieira <gioivieira@gmail.com>
2023-03-02 19:14:23 -0300

    Resolution of exercises 1 and 2.

---
+++ modulo7/testes-automatizados/tests/userPurchase.test.ts
@@ -0,0 +1,26 @@
+import userPurchase from "../src/testsFuncs/userPurchase"
+
+describe("Tests", () => {
+    const user = {
+        username: "Giovana",
+        userBalance: 50000
+    }
+
+    test("Test 1", () => {
+        expect(userPurchase(user, 30000)).toEqual({
+            username: user.username,
+            userBalance: user.userBalance - 30000
+        })
+    })
+
+    test("Test 2", () => {
+        expect(userPurchase(user, user.userBalance)).toEqual({
+            username: user.username,
+            userBalance: user.userBalance - user.userBalance
+        })
+    })
+
+    test("Test 3", () => {
+        expect(userPurchase(user, 60000)).toBe(undefined)
+    })
+})
